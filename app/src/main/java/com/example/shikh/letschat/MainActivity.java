166ac427caeef1ff220cca9624e1488ebf8fb857
Shikhar Tyagi <shikhart98@gmail.com>
2018-02-05 22:40:06 +0530

    new one
    Signed-off-by: Shikhar Tyagi <shikhart98@gmail.com>

--- app/src/main/java/com/example/shikh/letschat/MainActivity.java
+++ app/src/main/java/com/example/shikh/letschat/MainActivity.java
@@ -119,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.fingit dViewById(R.id.section_label);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
